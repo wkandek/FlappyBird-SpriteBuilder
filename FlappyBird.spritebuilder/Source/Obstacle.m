1355e74e2cc3d2fce62c8dfbde34df4fa67dc177
kandek <kandek@iMac27.local>
2014-06-27 06:53:35 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/Obstacle.m
+++ FlappyBird.spritebuilder/Source/Obstacle.m
@@ -33,7 +33,7 @@ static const CGFloat maximumYPosition = 380.f;
   CGFloat random = ((double)arc4random() / ARC4RANDOM_MAX);
   CGFloat range = maximumYPosition - minimumYPosition;
   // self.position = ccp(self.position.x, minimumYPosition + (random * range));
-  self.position = ccp(self.position.x, minimumYPosition + (range));
+  self.position = ccp(self.position.x, minimumYPosition + (0.5 * range));
 }
 
 @end
